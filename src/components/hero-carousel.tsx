b9ebc1d75b611a8a3d87573a5b77724be1ca67d0
Bima Ahida <bima.ahida@gmail.com>
2024-06-22 13:39:45 +0700

    feat: add function fetch data for banners

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -1,15 +1,37 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+
+import { queryHero, queryClient } from "@/services/bannersService";
+import { BannersType, Client, Hero } from "@/types/bannersType";
+import { fetch } from "@/services/sanity";
 
 // Import Assets //
 import carouselitem1 from "@/assets/images/img-dummy-1.jpg";
 import carouselitemlg from "@/assets/images/img-dummy-6.jpg";
 import logowhite from "@/assets/images/img-logowhite.png";
 
+async function fetchData(type: BannersType): Promise<Hero | Client> {
+	const query = type === "our-client" ? queryClient : queryHero;
+	return fetch(query);
+}
+
 export default function HeroCarousel() {
+	const [banners, setBanners] = useState<Image[]>([]);
+
+	useEffect(() => {
+		fetchData("hero").then((data) => {
+			const { banners } = data;
+			setBanners(banners)
+		}).catch((err) => console.error(err));
+	}, []);
+
+	console.log("banners", banners)
+	
 	return (
 		<Carousel
 			opts={{
