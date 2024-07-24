1d372294cbe8deea4aa631fdc2688c3a5381d623
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2024-07-24 12:10:21 +0700

    fix : Image resolution on About Section

--- src/components/about-carousel.tsx
+++ src/components/about-carousel.tsx
@@ -12,161 +12,182 @@ import { queryAbout } from "@/services/bannersService";
 import { fetch } from "@/services/sanity";
 
 export default function AboutCarousel() {
-  const [banners, setBanners] = useState<ImageType[]>([]);
-  const hasFetched = useRef(false);
+	const [banners, setBanners] = useState<ImageType[]>([]);
+	const hasFetched = useRef(false);
 
-  useEffect(() => {
-    if (!hasFetched.current) {
-      fetch(queryAbout)
-        .then((data) => {
-          const { banners } = data;
-          setBanners(banners);
-        })
-        .catch((err) => console.error(err));
-      hasFetched.current = true;
-    }
-  }, []);
+	useEffect(() => {
+		if (!hasFetched.current) {
+			fetch(queryAbout)
+				.then((data) => {
+					const { banners } = data;
+					setBanners(banners);
+				})
+				.catch((err) => console.error(err));
+			hasFetched.current = true;
+		}
+	}, []);
 
-  const imageLabel = [
-    "Innovation",
-    "Design Excellence",
-    "Collaborative Approach",
-  ];
+	const imageLabel = [
+		"Innovation",
+		"Design Excellence",
+		"Collaborative Approach",
+	];
 
-  const renderCarousel = () => (
-    <Carousel
-      opts={{
-        align: "start",
-        loop: true,
-      }}
-      plugins={[
-        Autoplay({
-          delay: 3000,
-        }),
-      ]}
-      className="w-full h-full"
-    >
-      <CarouselContent>
-        {banners?.map((banner, index) => (
-          <CarouselItem
-            key={`banner-${index}`}
-            className="block w-full h-full aspect-square lg:aspect-auto"
-          >
-            <Image
-              src={banner.url}
-              alt={`about-image-${index}`}
-              priority={true}
-              width={1000}
-              height={1000}
-              className="object-cover object-center w-full h-full"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
-  );
+	const renderCarousel = () => (
+		<Carousel
+			opts={{
+				align: "start",
+				loop: true,
+			}}
+			plugins={[
+				Autoplay({
+					delay: 3000,
+				}),
+			]}
+			className="w-full h-full"
+		>
+			<CarouselContent>
+				{banners?.map((banner, index) => (
+					<CarouselItem
+						key={`banner-${index}`}
+						className="block w-full h-full aspect-square lg:aspect-auto"
+					>
+						<Image
+							src={banner.url}
+							alt={`about-image-${index}`}
+							priority={true}
+							width={0}
+							height={0}
+							sizes="100vw"
+							quality={100}
+							className="object-cover object-center w-full h-full"
+						/>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+		</Carousel>
+	);
 
-  return (
-    <>
-      <div className="hidden lg:block">
-        <div className="relative">
-          <div className="absolute inset-0 flex px-36 2xl:px-[102px] items-start">
-            <p className="text-lg text-black [writing-mode:vertical-rl] tracking-wider rotate-180 leading-none">
-              About us
-            </p>
-          </div>
-          <div className="flex items-center w-full gap-4 justify-center px-sectionpxlg 2xl:px-sectionpx2xl">
-            {imageLabel.map((label, index) => (
-              <div className="relative w-full" key={`key-${label}`}>
-                <div className="absolute inset-0 items-end flex px-7 pb-7">
-                  <p className="text-xl leading-none text-white">{label}</p>
-                </div>
-                {banners[index]?.url ? (
-                  <Image
-                    src={banners[index].url}
-                    alt={`about-image-${label}`}
-                    priority={true}
-                    width={1000}
-                    height={1000}
-                    className="w-full h-[210px] object-cover"
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
+	return (
+		<>
+			<div className="hidden lg:block">
+				<div className="relative">
+					<div className="absolute inset-0 flex px-36 2xl:px-[102px] items-start">
+						<p className="text-lg text-black [writing-mode:vertical-rl] tracking-wider rotate-180 leading-none">
+							About us
+						</p>
+					</div>
+					<div className="flex items-center w-full gap-4 justify-center px-sectionpxlg 2xl:px-sectionpx2xl">
+						{imageLabel.map((label, index) => (
+							<div
+								className="relative w-full"
+								key={`key-${label}`}
+							>
+								<div className="absolute inset-0 items-end flex px-7 pb-7">
+									<p className="text-xl leading-none text-white">
+										{label}
+									</p>
+								</div>
+								{banners[index]?.url ? (
+									<Image
+										src={banners[index].url}
+										alt={`about-image-${label}`}
+										priority={true}
+										width={0}
+										height={0}
+										sizes="100vw"
+										quality={100}
+										className="w-full h-[210px] object-cover"
+									/>
+								) : (
+									""
+								)}
+							</div>
+						))}
+					</div>
+				</div>
 
-        <div className="px-sectionpxlg 2xl:px-sectionpx2xl w-full mt-4">
-          {renderCarousel()}
-        </div>
+				<div className="px-sectionpxlg 2xl:px-sectionpx2xl w-full mt-4">
+					{renderCarousel()}
+				</div>
 
-        <div className="px-sectionpxlg 2xl:px-sectionpx2xl w-full mt-32 flex flex-col gap-3">
-          <div className="w-full py-3 border-b-[1px] border-black border-opacity-20">
-            <div className="w-fit">
-              <Link href="/project">
-                <p className="text-[26px] text-black cursor-pointer">Project</p>
-              </Link>
-            </div>
-          </div>
-          <div className="w-full py-3 border-b-[1px] border-black border-opacity-20">
-            <div className="w-fit">
-              <Link href="/contact">
-                <p className="text-[26px] text-black cursor-pointer">Contact</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+				<div className="px-sectionpxlg 2xl:px-sectionpx2xl w-full mt-32 flex flex-col gap-3">
+					<div className="w-full py-3 border-b-[1px] border-black border-opacity-20">
+						<div className="w-fit">
+							<Link href="/project">
+								<p className="text-[26px] text-black cursor-pointer">
+									Project
+								</p>
+							</Link>
+						</div>
+					</div>
+					<div className="w-full py-3 border-b-[1px] border-black border-opacity-20">
+						<div className="w-fit">
+							<Link href="/contact">
+								<p className="text-[26px] text-black cursor-pointer">
+									Contact
+								</p>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
 
-      <div className="block lg:hidden">
-        <div className="flex gap-6 items-start">
-          <p className="text-lg text-black [writing-mode:vertical-rl] rotate-180 leading-none">
-            About us
-          </p>
-          <div className="w-full flex flex-col gap-5">
-            {imageLabel.map((label, index) => (
-              <div className="relative" key={`image-${label}`}>
-                <div className="absolute inset-0 items-end flex px-7 pb-7">
-                  <p className="text-xl leading-none text-white">{label}</p>
-                </div>
+			<div className="block lg:hidden">
+				<div className="flex gap-6 items-start">
+					<p className="text-lg text-black [writing-mode:vertical-rl] rotate-180 leading-none">
+						About us
+					</p>
+					<div className="w-full flex flex-col gap-5">
+						{imageLabel.map((label, index) => (
+							<div className="relative" key={`image-${label}`}>
+								<div className="absolute inset-0 items-end flex px-7 pb-7">
+									<p className="text-xl leading-none text-white">
+										{label}
+									</p>
+								</div>
 
-                {banners[index]?.url ? (
-                  <Image
-                    src={banners[index].url}
-                    alt={`about-image-${label}`}
-                    priority={true}
-                    width={1000}
-                    height={1000}
-                    className="w-full h-[200px] object-cover"
-                  />
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-            {renderCarousel()}
-          </div>
-        </div>
+								{banners[index]?.url ? (
+									<Image
+										src={banners[index].url}
+										alt={`about-image-${label}`}
+										priority={true}
+										width={0}
+										height={0}
+										sizes="100vw"
+										quality={100}
+										className="w-full h-[200px] object-cover"
+									/>
+								) : (
+									""
+								)}
+							</div>
+						))}
+						{renderCarousel()}
+					</div>
+				</div>
 
-        <div className="w-full pt-16">
-          <div className="w-full py-4 border-b-[1px] border-black border-opacity-20">
-            <div className="w-fit">
-              <Link href="/project">
-                <p className="text-[26px] text-black">Project</p>
-              </Link>
-            </div>
-          </div>
-          <div className="w-full py-4 border-b-[1px] border-black border-opacity-20">
-            <div className="w-fit">
-              <Link href="/contact">
-                <p className="text-[26px] text-black">Contact</p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+				<div className="w-full pt-16">
+					<div className="w-full py-4 border-b-[1px] border-black border-opacity-20">
+						<div className="w-fit">
+							<Link href="/project">
+								<p className="text-[26px] text-black">
+									Project
+								</p>
+							</Link>
+						</div>
+					</div>
+					<div className="w-full py-4 border-b-[1px] border-black border-opacity-20">
+						<div className="w-fit">
+							<Link href="/contact">
+								<p className="text-[26px] text-black">
+									Contact
+								</p>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
