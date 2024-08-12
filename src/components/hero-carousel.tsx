6e0578ff8d447dad3791c7766b3663b31481c42d
Bima Ahida <bima.ahida@gmail.com>
2024-08-13 01:28:22 +0700

    fix: handle full duration slider on hero carousel

--- src/components/hero-carousel.tsx
+++ src/components/hero-carousel.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useEffect, useState, useRef } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, SwiperRef } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
 //  Import Services
@@ -16,170 +16,134 @@ import "swiper/css";
 import RenderAsset from "./render-asset";
 
 interface Banner {
-	url: string;
+  url: string;
 }
 
 export default function HeroCarousel() {
-	const [banners, setBanners] = useState<Banner[]>([]);
-	const hasFetched = useRef(false);
-	const [swiperRef, setSwiperRef] = useState<any>(null);
-	const [current, setCurrent] = useState(1);
-	const videoRefs = useRef<React.RefObject<HTMLVideoElement>[]>([]);
+  const hasFetched = useRef(false);
+  const swiperRef = useRef<SwiperRef>(null);
 
-	useEffect(() => {
-		if (!hasFetched.current) {
-			fetch(queryHero)
-				.then((data) => setBanners(data.banners))
-				.catch((err) => console.error(err));
-			hasFetched.current = true;
-		}
-	}, []);
+  const [banners, setBanners] = useState<Banner[]>([]);
+  const [current, setCurrent] = useState(1);
 
-	useEffect(() => {
-		if (!swiperRef) {
-			return;
-		}
+  const videoRefs = useRef<React.RefObject<HTMLVideoElement>[]>([]);
 
-		let isManualSlide = false;
-		let timeoutId: NodeJS.Timeout | null = null;
+  //   let timeoutId: NodeJS.Timeout[] = [];
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-		const muteAllVideos = () => {
-			videoRefs.current.forEach((videoRef) => {
-				if (videoRef.current) {
-					videoRef.current.muted = true;
-				}
-			});
-		};
+  useEffect(() => {
+    if (!hasFetched.current) {
+      fetch(queryHero)
+        .then((data) => setBanners(data.banners))
+        .catch((err) => console.error(err));
+      hasFetched.current = true;
+    }
+  }, []);
 
-		const onSlideChange = () => {
-			isManualSlide = swiperRef.touches.diff !== 0;
+  const muteAllVideos = () => {
+    videoRefs.current.forEach((videoRef) => {
+      if (videoRef.current) {
+        videoRef.current.muted = true;
+      }
+    });
+  };
 
-			if (timeoutId) {
-				clearTimeout(timeoutId);
-			}
+  const swapSlideNext = () => {
+    swiperRef?.current?.swiper.slideNext();
+  };
 
-			muteAllVideos();
+  return (
+    <Swiper
+      ref={swiperRef}
+      modules={[Autoplay]}
+      loop={true}
+      onSlideChange={(swiper) => {
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
 
-			videoRefs.current.forEach((videoRef, index) => {
-				if (videoRef.current) {
-					videoRef.current.pause();
-					videoRef.current.currentTime = 1;
+        muteAllVideos();
 
-					videoRef.current.classList.remove(
-						"min-h-[16rem]",
-						"max-h-[16rem]",
-						"lg:min-h-[49rem]",
-						"lg:max-h-[49rem]",
-						"2xl:min-h-[50rem]",
-						"2xl:max-h-[50rem]"
-					);
-					videoRef.current.classList.add(
-						"min-h-[100vh]",
-						"max-h-[100vh]"
-					);
+        const videoRef = videoRefs.current[swiper.realIndex];
+        if (videoRef && videoRef.current) {
+          videoRef.current.currentTime = 1;
+          videoRef.current.classList.remove(
+            "min-h-[16rem]",
+            "max-h-[16rem]",
+            "lg:min-h-[49rem]",
+            "lg:max-h-[49rem]",
+            "2xl:min-h-[50rem]",
+            "2xl:max-h-[50rem]"
+          );
+          videoRef.current.classList.add("min-h-[100vh]", "max-h-[100vh]");
 
-					if (!isManualSlide && swiperRef.realIndex === index) {
-						const isVideoSlide =
-							videoRef.current.tagName === "VIDEO";
+          videoRef.current
+            .play()
+            .then(() => {
+              if (videoRef.current) {
+                videoRef.current.onended = () => swapSlideNext();
+              }
+            })
+            .catch((err) => console.error("Video playback failed", err));
+        } else {
+          timeoutRef.current = setTimeout(swapSlideNext, 12000); // set default delay 12s
+        }
 
-						if (isVideoSlide) {
-							videoRef.current
-								.play()
-								.then(() => {
-									if (videoRef.current) {
-										videoRef.current.onended = () => {
-											swiperRef.slideNext();
-										};
-									}
-								})
-								.catch((err) =>
-									console.error("Video playback failed", err)
-								);
-						} else {
-							timeoutId = setTimeout(() => {
-								swiperRef.slideNext();
-							}, 12000);
-						}
-					}
-				}
-			});
-			setCurrent(swiperRef.realIndex + 1);
-		};
+        setCurrent(swiper.realIndex + 1);
+      }}
+      className="w-full h-screen"
+    >
+      {banners?.map((banner, index) => {
+        if (!banner?.url) {
+          return "";
+        }
 
-		swiperRef.on("slideChange", onSlideChange);
+        if (!videoRefs.current[index]) {
+          videoRefs.current[index] = React.createRef<HTMLVideoElement>();
+        }
 
-		return () => {
-			if (timeoutId) {
-				clearTimeout(timeoutId);
-			}
-			swiperRef.off("slideChange", onSlideChange);
-		};
-	}, [swiperRef]);
+        return (
+          <SwiperSlide
+            key={`banner-${index}`}
+            className="block w-full object-cover"
+          >
+            <div className="relative">
+              <RenderAsset
+                url={banner.url}
+                imageClassName="object-cover object-center h-screen w-full"
+                imageAlt={`banner-image-${index}`}
+                videoClassName="object-cover object-center h-screen w-full"
+                videoAutoPlay={true}
+                videoLoop={false}
+                videoMute={true}
+                showButton={true}
+                ignoreAspectRatio={true}
+                videoRef={videoRefs.current[index]}
+              />
 
-	return (
-		<Swiper
-			modules={[Autoplay]}
-			loop={true}
-			autoplay={{
-				delay: 12000,
-				disableOnInteraction: false,
-				waitForTransition: true,
-			}}
-			onSwiper={setSwiperRef}
-			className="w-full h-screen"
-		>
-			{banners?.map((banner, index) => {
-				if (!banner?.url) {
-					return "";
-				}
-
-				if (!videoRefs.current[index]) {
-					videoRefs.current[index] =
-						React.createRef<HTMLVideoElement>();
-				}
-
-				return (
-					<SwiperSlide
-						key={`banner-${index}`}
-						className="block w-full object-cover"
-					>
-						<div className="relative">
-							<RenderAsset
-								url={banner.url}
-								imageClassName="object-cover object-center h-screen w-full"
-								imageAlt={`banner-image-${index}`}
-								videoClassName="object-cover object-center h-screen w-full"
-								videoAutoPlay={true}
-								videoLoop={false}
-								videoMute={true}
-								showButton={true}
-								ignoreAspectRatio={true}
-								videoRef={videoRefs.current[index]}
-							/>
-
-							<div
-								className="absolute flex justify-center w-full px-sectionpxsm lg:px-sectionpxlg 2xl:px-sectionpx2xl items-end pb-10 lg:pb-[70px]"
-								style={{
-									height: "fit-content",
-									alignSelf: "flex-end",
-									bottom: 0,
-								}}
-							>
-								<div className="w-full flex items-end bg-white h-[1px] bg-opacity-70">
-									<div className="flex w-full">
-										{banners.map((_, index) => (
-											<div
-												key={index}
-												className={`h-[5px] flex-1 ${current - 1 === index ? "bg-white" : ""}`}
-											></div>
-										))}
-									</div>
-								</div>
-							</div>
-						</div>
-					</SwiperSlide>
-				);
-			})}
-		</Swiper>
-	);
+              <div
+                className="absolute flex justify-center w-full px-sectionpxsm lg:px-sectionpxlg 2xl:px-sectionpx2xl items-end pb-10 lg:pb-[70px]"
+                style={{
+                  height: "fit-content",
+                  alignSelf: "flex-end",
+                  bottom: 0,
+                }}
+              >
+                <div className="w-full flex items-end bg-white h-[1px] bg-opacity-70">
+                  <div className="flex w-full">
+                    {banners.map((_, index) => (
+                      <div
+                        key={index}
+                        className={`h-[5px] flex-1 ${current - 1 === index ? "bg-white" : ""}`}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 }
