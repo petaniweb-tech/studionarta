6a7a038a6ca7f877c5221777f055a01d2096c673
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2024-09-30 13:45:30 +0700

    docs: formatted the corresponding files

--- src/components/render-asset.tsx
+++ src/components/render-asset.tsx
@@ -4,58 +4,58 @@ import Image from "next/image";
 import VideoPlayer from "./video-player";
 
 const RenderAsset: React.FC<{
-	url: string;
-	imageAlt: string;
-	imageClassName: string;
-	videoClassName: string;
-	videoAutoPlay: boolean;
-	videoMute: boolean;
-	videoLoop: boolean;
-	showButton?: boolean;
-	videoRef?: React.RefObject<HTMLVideoElement>;
+  url: string;
+  imageAlt: string;
+  imageClassName: string;
+  videoClassName: string;
+  videoAutoPlay: boolean;
+  videoMute: boolean;
+  videoLoop: boolean;
+  showButton?: boolean;
+  videoRef?: React.RefObject<HTMLVideoElement>;
 }> = ({
-	url,
-	imageAlt,
-	imageClassName,
-	videoClassName,
-	videoAutoPlay,
-	videoLoop,
-	videoMute,
-	showButton = true,
-	videoRef,
+  url,
+  imageAlt,
+  imageClassName,
+  videoClassName,
+  videoAutoPlay,
+  videoLoop,
+  videoMute,
+  showButton = true,
+  videoRef,
 }) => {
-	const type = whatIsType(url);
+  const type = whatIsType(url);
 
-	if (type === "image") {
-		return (
-			<Image
-				src={url}
-				alt={imageAlt}
-				priority={true}
-				width={0}
-				height={0}
-				sizes="100vw"
-				quality={100}
-				className={imageClassName}
-			/>
-		);
-	}
+  if (type === "image") {
+    return (
+      <Image
+        src={url}
+        alt={imageAlt}
+        priority={true}
+        width={0}
+        height={0}
+        sizes="100vw"
+        quality={100}
+        className={imageClassName}
+      />
+    );
+  }
 
-	if (type === "video") {
-		return (
-			<VideoPlayer
-				url={url}
-				autoPlay={videoAutoPlay}
-				muted={videoMute}
-				loop={videoLoop}
-				className={videoClassName}
-				showButton={showButton}
-				videoRef={videoRef}
-			/>
-		);
-	}
+  if (type === "video") {
+    return (
+      <VideoPlayer
+        url={url}
+        autoPlay={videoAutoPlay}
+        muted={videoMute}
+        loop={videoLoop}
+        className={videoClassName}
+        showButton={showButton}
+        videoRef={videoRef}
+      />
+    );
+  }
 
-	return null;
+  return null;
 };
 
 export default RenderAsset;
