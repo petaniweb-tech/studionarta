583b01b6c24e73ef2f6eb4f948d77693f2671650
ilyas-muhammad <ell@petaniweb.com>
2024-11-18 19:02:38 +0700

    feat: update contact info in corporate project modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -79,67 +79,80 @@ export default function Modal() {
 	}
 
 	return (
-		<AlertDialog open={open} onOpenChange={setOpen}>
-			<AlertDialogContent>
-				<AlertDialogTitle>Enter your code</AlertDialogTitle>
-				<AlertDialogDescription>
-					Please enter your unique code to continue
-				</AlertDialogDescription>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogTitle>Enter your code</AlertDialogTitle>
+        <AlertDialogDescription>
+          Please enter your unique code to continue
+        </AlertDialogDescription>
 
-				<InputOTP
-					maxLength={6}
-					pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
-					onChange={(value) => setCode(value.toUpperCase())}
-					inputMode="text"
-				>
-					<InputOTPGroup className="mt-6">
-						<InputOTPSlot index={0} />
-						<InputOTPSlot index={1} />
-						<InputOTPSlot index={2} />
-						<InputOTPSlot index={3} />
-						<InputOTPSlot index={4} />
-						<InputOTPSlot index={5} />
-					</InputOTPGroup>
-				</InputOTP>
+        <InputOTP
+          maxLength={6}
+          pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+          onChange={(value) => setCode(value.toUpperCase())}
+          inputMode="text"
+        >
+          <InputOTPGroup className="mt-6">
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
 
-				{error && (
-					<div className="w-full mt-2 rounded bg-red-500 bg-opacity-[0.08] border border-red-500 border-opacity-20 flex items-center justify-center pt-[7px] pb-2">
-						<p className="text-red-500 text-sm text-center font-supportingfont">
-							{error}
-						</p>
-					</div>
-				)}
+        {error && (
+          <div className="w-full mt-2 rounded bg-red-500 bg-opacity-[0.08] border border-red-500 border-opacity-20 flex items-center justify-center pt-[7px] pb-2">
+            <p className="text-red-500 text-sm text-center font-supportingfont">
+              {error}
+            </p>
+          </div>
+        )}
 
-				<AlertDialogAction
-					onClick={(e) => {
-						e.preventDefault();
-						handleSubmit();
-					}}
-					className="mt-6 text-lg font-semibold py-6"
-				>
-					Submit
-				</AlertDialogAction>
+        <AlertDialogAction
+          onClick={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          className="mt-6 text-lg font-semibold py-6"
+        >
+          Submit
+        </AlertDialogAction>
 
-				<AlertDialogDescription className="mt-7 leading-[1.58] text-wrap">
-					For information regarding our Corporate Projects please{" "}
-					<br className="hidden lg:block" />
-					contact our support
-				</AlertDialogDescription>
+        <AlertDialogDescription className="mt-7 leading-[1.58] text-wrap">
+          For information regarding our Corporate Projects please{" "}
+          <br className="hidden lg:block" />
+          contact our support
+        </AlertDialogDescription>
 
-				<div className="flex items-center justify-center w-full mt-3">
-					<Link
-						href="https://wa.me/628123024660"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<AlertDialogDescription className="pb-[3px] px-[1px] border-b-[0.5px] border-neutral-500 w-fit cursor-pointer group hover:border-white duration-300">
-							<span className="text-neutral-300 group-hover:text-white duration-300">
-								Devina +62 812 3024 660
-							</span>
-						</AlertDialogDescription>
-					</Link>
-				</div>
-			</AlertDialogContent>
-		</AlertDialog>
-	);
+        <div className="flex items-center justify-center w-full mt-3">
+          <Link
+            href="https://wa.me/628123024660"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AlertDialogDescription className="pb-[3px] px-[1px] border-b-[0.5px] border-neutral-500 w-fit cursor-pointer group hover:border-white duration-300">
+              <span className="text-neutral-300 group-hover:text-white duration-300">
+                CP 01 - Devina +62 812 3024 660
+              </span>
+            </AlertDialogDescription>
+          </Link>
+        </div>
+        <div className="flex items-center justify-center w-full">
+          <Link
+            href="https://wa.me/6281803383993"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AlertDialogDescription className="pb-[3px] px-[1px] border-b-[0.5px] border-neutral-500 w-fit cursor-pointer group hover:border-white duration-300">
+              <span className="text-neutral-300 group-hover:text-white duration-300">
+                CP 02 - Dio +62 818 0338 3993
+              </span>
+            </AlertDialogDescription>
+          </Link>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
